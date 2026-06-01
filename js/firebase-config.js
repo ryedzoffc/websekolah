28e8ba43d788ff4e1fefe476e62482fee2e7a844
ryedzoffc <edimekey@gmail.com>
2026-06-01 12:43:50 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,30 +1,15 @@
 // js/firebase-config.js
-// ============================================
-// KONFIGURASI FIREBASE - TANPA ES MODULES
-// SMK TI Bali Global Karangasem
-// ============================================
-
-// Inisialisasi Firebase menggunakan CDN (sudah include di HTML)
-// Jadi file ini hanya untuk konfigurasi global
-
-// Konfigurasi Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyBMdBOguRVd02u79idp4xAoTfjDy-h28EQ",
     authDomain: "osisedimahendra.firebaseapp.com",
     projectId: "osisedimahendra",
     storageBucket: "osisedimahendra.firebasestorage.app",
     messagingSenderId: "382159139986",
-    appId: "1:382159139986:web:f33b3470b1136ccf50a24b",
-    measurementId: "G-T7ESGSCSGQ"
+    appId: "1:382159139986:web:f33b3470b1136ccf50a24b"
 };
 
-// Inisialisasi Firebase
 firebase.initializeApp(firebaseConfig);
-
-// Inisialisasi Firestore
 const db = firebase.firestore();
-
-// Inisialisasi Auth
 const auth = firebase.auth();
 
-console.log('Firebase berhasil diinisialisasi');
+console.log('✅ Firebase ready');
